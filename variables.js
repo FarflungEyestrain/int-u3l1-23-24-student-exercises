0def834e726aa1e7dcba9107bf1a3beec5f7e3b8
FarflungEyestrain <stedgegavin@gmail.com>
2024-06-11 10:27:07 -0400

    commit

--- variables.js
+++ variables.js
@@ -1,11 +1,11 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+let favoriteColor= "Black";
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood= "Pizza";
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber= 69;
 
 
 
@@ -13,6 +13,5 @@ let favoriteColor;
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
 // - Use a string value of your favorite food.
-// - Use a number value of your favorite number.
-
+// - Use a number value of your favorite number
 // 4. Console log each variable on a new line.
